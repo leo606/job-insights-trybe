5ee9da859bd6c4ca45ce08b8d221bdf4678a2f71
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-11 20:35:26 -0300

    sort

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,7 +1,7 @@
 import pytest
 from src.sorting import sort_by
 
-jobs = [
+MOCK_JOBS = [
     {"max_salary": 0, "min_salary": 10, "date_posted": "2020-04-20"},
     {"max_salary": 10, "min_salary": 100, "date_posted": "2020-04-21"},
     {"max_salary": 10000, "min_salary": 200, "date_posted": "2020-04-22"},
@@ -13,6 +13,27 @@ jobs = [
 criterias = ("min_salary", "max_salary", "date_posted")
 
 
-def test_sort_by_criteria():
+@pytest.fixture
+def jobs():
+    return [
+        {"max_salary": 0, "min_salary": 10, "date_posted": "2020-04-20"},
+        {"max_salary": 10, "min_salary": 100, "date_posted": "2020-04-21"},
+        {"max_salary": 10000, "min_salary": 200, "date_posted": "2020-04-22"},
+        {"max_salary": 15000, "min_salary": 4, "date_posted": "2020-04-23"},
+        {"max_salary": 1500, "min_salary": 0, "date_posted": "2020-04-24"},
+        {"max_salary": -1, "min_salary": 10, "date_posted": "2020-04-25"},
+    ]
+
+
+def test_sort_by_criteria(jobs):
     with pytest.raises(ValueError, match="invalid sorting criteria: invalid"):
         assert sort_by(jobs, criteria="invalid")
+
+    sort_by_min_salary = sort_by(jobs, criterias[0])
+    assert sort_by_min_salary[0] == MOCK_JOBS[4]
+
+    sort_by_max_salary = sort_by(jobs, criterias[1])
+    assert sort_by_max_salary[0] == MOCK_JOBS[3]
+
+    sort_by_date_posted = sort_by(jobs, criterias[2])
+    assert sort_by_date_posted[0] == MOCK_JOBS[5]
